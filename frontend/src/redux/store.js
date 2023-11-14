933e5743d248787753b51fc54f4565181ef82ba5
AlonaAstakhova <alyonasta@gmail.com>
2023-11-14 11:59:41 -0800

    feat: filter Redux slice and filter by title

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import bookReducer from './books/reducer'
+import filterReducer from './slices/filterSlice'
 
 const store = configureStore({
-  reducer: { books: bookReducer },
+  reducer: { books: bookReducer, filter: filterReducer },
 })
 
 export default store
