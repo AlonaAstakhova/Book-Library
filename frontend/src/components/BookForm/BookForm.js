e98ebe7f1a0b87c1b49f57d57c9767cf50ae0f7d
AlonaAstakhova <alyonasta@gmail.com>
2023-11-15 18:09:29 -0800

    refactor: added cteateAsyncThunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice.js'
+import { addBook, fetchBook } from '../../redux/slices/booksSlice.js'
 import booksData from '../../data/books.json'
 import createBookWithID from '../../utils/createBookWithID.js'
 import './BookForm.css'
@@ -26,7 +26,7 @@ const BookForm = () => {
   }
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(thunkFunction)
+    dispatch(fetchBook())
   }
 
   return (
